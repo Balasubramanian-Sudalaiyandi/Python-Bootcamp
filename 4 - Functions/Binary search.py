5dba11f8234e520f0c4b6069cc989655fa84df1b
Balasubramanian-Sudalaiyandi <bala281197@gmail.com>
2025-07-19 23:34:10 +0530

    Adding package

---
+++ 4 - Functions/Binary search.py
@@ -0,0 +1,32 @@
+def binary_search(arr,target):
+
+    size = len(arr)
+
+    start = 0
+    end = size-1
+
+    while start <= end:
+        mid = (start+end) // 2
+        if arr[mid] == target:
+            return mid
+        elif (arr[mid] > target):
+            end = mid -1
+        elif (arr[mid] < target):
+            start = mid + 1
+
+    return -1
+        
+sorted_list = [10,23,37,45,50,70,95]
+target = 70
+
+result = binary_search(sorted_list,target)
+
+print(result) 
+
+
+
+
+
+
+
+
